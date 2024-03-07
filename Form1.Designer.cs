c492dab58d662005934fb52533b965394845605d
JohannGaviria <johannmgr@gmail.com>
2024-03-06 21:18:38 -0500

    Creación del programa

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace aplicacionBroma
+{
+    partial class frmBroma
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.btnSi = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.lblFormateando = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.Location = new System.Drawing.Point(247, 66);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(165, 13);
+            this.lblTexto.TabIndex = 0;
+            this.lblTexto.Text = "¿DESEAS FORMATEAR LA PC?";
+            this.lblTexto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSi
+            // 
+            this.btnSi.Location = new System.Drawing.Point(141, 252);
+            this.btnSi.Name = "btnSi";
+            this.btnSi.Size = new System.Drawing.Size(75, 23);
+            this.btnSi.TabIndex = 1;
+            this.btnSi.Text = "SI";
+            this.btnSi.UseVisualStyleBackColor = true;
+            this.btnSi.Click += new System.EventHandler(this.btnSi_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(435, 252);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(75, 23);
+            this.btnNo.TabIndex = 2;
+            this.btnNo.Text = "NO";
+            this.btnNo.UseVisualStyleBackColor = true;
+            // 
+            // lblFormateando
+            // 
+            this.lblFormateando.AutoSize = true;
+            this.lblFormateando.Location = new System.Drawing.Point(273, 164);
+            this.lblFormateando.Name = "lblFormateando";
+            this.lblFormateando.Size = new System.Drawing.Size(116, 13);
+            this.lblFormateando.TabIndex = 3;
+            this.lblFormateando.Text = "FORMATEANDO PC...";
+            this.lblFormateando.Visible = false;
+            // 
+            // frmBroma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFormateando);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnSi);
+            this.Controls.Add(this.lblTexto);
+            this.Name = "frmBroma";
+            this.Text = "Aplicacion Broma";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmBroma_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTexto;
+        private System.Windows.Forms.Button btnSi;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Label lblFormateando;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+// 27 - Feb - 2024
+// Johann Mauricio Gaciria Rodriguez
+// este programa crea una experiencia interactiva donde el botón btnNo se mueve
+// aleatoriamente por el formulario cuando el ratón se acerca a él, y al hacer clic en
+// el botón btnSi, se revela un mensaje, como una broma para el usuario.
+
+
+namespace aplicacionBroma
+{
+    public partial class frmBroma : Form
+    {
+        private Random random = new Random();
+
+        public frmBroma()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveButtonToRandomLocation()
+        {
+            int x = random.Next(0, ClientSize.Width - btnNo.Width);
+            int y = random.Next(0, ClientSize.Height - btnNo.Height);
+            btnNo.Location = new Point(x, y);
+        }
+
+        private void frmBroma_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsMouseNearButton(e.Location, btnNo))
+            {
+                MoveButtonToRandomLocation();
+            }
+        }
+
+        private bool IsMouseNearButton(Point mouseLocation, Control control)
+        {
+            Point controlCenter = new Point(control.Left + control.Width / 2, control.Top + control.Height / 2);
+            double distance = Math.Sqrt(Math.Pow(mouseLocation.X - controlCenter.X, 2) + Math.Pow(mouseLocation.Y - controlCenter.Y, 2));
+
+            double threshold = 100;
+
+            return distance < threshold;
+        }
+
+        private void btnSi_Click(object sender, EventArgs e)
+        {
+            lblFormateando.Visible = true;
+        }
+
+    }
+}
